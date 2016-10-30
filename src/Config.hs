4659f4c348018393a9836bd92bdeda05b93f3d2d
Jun <me@noraesae.net>
2016-10-30 16:37:35 +0900

    Implement Config module to parse YAML config

--- src/Config.hs
+++ src/Config.hs
@@ -1,8 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Config
   ( getPort
   ) where
 
+import Cmd (getConfigPath)
+import Data.Yaml (FromJSON, decodeFile)
+import GHC.Generics
+import System.Exit (exitFailure)
+
 type Port = Int -- in Network.Wai.Handler.Warp
 
+data Config = Config { port :: Port
+                     } deriving Generic
+
+instance FromJSON Config
+
+config :: IO Config
+config = do
+  config <- getConfigPath >>= decodeFile
+  case config of
+    Just x -> return x
+    Nothing -> putStrLn "invalid config" >> exitFailure
+
 getPort :: IO Port
-getPort = return 8080 -- FIXME
+getPort = port <$> config
