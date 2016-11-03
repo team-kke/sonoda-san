f97f07ef7b7eefe049fb9aae942384f8682fe15d
Jun <me@noraesae.net>
2016-11-04 01:07:10 +0900

    Add LINE channel settings to config

--- src/Config.hs
+++ src/Config.hs
@@ -2,9 +2,12 @@
 
 module Config (
   getPort,
+  getChannelSecret,
+  getChannelAccessToken,
   ) where
 
 import Cmd (getConfigPath)
+import Data.Text (Text)
 import Data.Yaml (FromJSON, decodeFile)
 import GHC.Generics
 import System.Exit (exitFailure)
@@ -12,6 +15,8 @@ import System.Exit (exitFailure)
 type Port = Int -- in Network.Wai.Handler.Warp
 
 data Config = Config { port :: Port
+                     , channelSecret :: Text
+                     , channelAccessToken :: Text
                      } deriving Generic
 
 instance FromJSON Config
@@ -25,3 +30,9 @@ config = do
 
 getPort :: IO Port
 getPort = port <$> config
+
+getChannelSecret :: IO Text
+getChannelSecret = channelSecret <$> config
+
+getChannelAccessToken :: IO Text
+getChannelAccessToken = channelAccessToken <$> config
