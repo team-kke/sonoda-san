119688a8fd4951dc16a9d7959646860662e44f12
Jun <me@noraesae.net>
2016-11-20 13:54:42 +0900

    Remove line API part from src
    It's moved to https://github.com/noraesae/line

--- src/Line/Messaging/Types.hs
+++
@@ -1,9 +0,0 @@
-module Line.Messaging.Types (
-  module Line.Messaging.API.Types,
-  module Line.Messaging.Common.Types,
-  module Line.Messaging.Webhook.Types,
-  ) where
-
-import Line.Messaging.API.Types
-import Line.Messaging.Common.Types
-import Line.Messaging.Webhook.Types
