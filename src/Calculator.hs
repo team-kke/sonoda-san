7a862d2c63916cfd57e6eb7937cb0e4f47ccdefc
Jun <me@noraesae.net>
2016-11-20 16:19:54 +0900

    Implement simple calculator
    Only for add and mul.

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -2,10 +2,42 @@ module Calculator (
   calc,
   ) where
 
+import Control.Applicative ((<|>))
+import Data.Attoparsec.ByteString
+import Data.Attoparsec.ByteString.Char8 (decimal, char, skipSpace)
+import qualified Data.ByteString as B
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 
 calc :: T.Text -> T.Text
-calc = T.pack . show . calcInternal . T.unpack
+calc x = T.pack $ case calcInternal . TE.encodeUtf8 $ x of
+  Right result -> show result
+  Left err -> err
 
-calcInternal :: String -> Integer
-calcInternal = undefined
+calcInternal :: B.ByteString -> Either String Integer
+calcInternal = parseOnly expr
+
+expr :: Parser Integer
+expr = addExpr
+
+addExpr :: Parser Integer
+addExpr = do
+  a <- mulExpr
+  skipSpace
+  b <- plus <|> return 0
+  return $ a + b
+    where plus = do
+            _ <- char '+'
+            skipSpace
+            expr
+
+mulExpr :: Parser Integer
+mulExpr = do
+  a <- decimal
+  skipSpace
+  b <- mul <|> return 1
+  return $ a * b
+    where mul = do
+            _ <- char '*'
+            skipSpace
+            decimal
